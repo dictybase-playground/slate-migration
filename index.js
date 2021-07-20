bc075f30001dbb775410a5fef11551a0c7122e99
wildlifehexagon <hartline.eric@gmail.com>
2021-07-20 16:07:01 -0500

    feat: add yargs for cli parsing

--- index.js
+++ index.js
@@ -1,21 +1,68 @@
-import { getSlugsWithContentList, updateContent, pool } from "./queries.js";
+#!/usr/bin/env node
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import pg from "pg";
+import { getSlugsWithContentList, updateContent } from "./queries.js";
 import { convertSlate047 } from "./convert.js";
 
-const main = async () => {
+const { Pool } = pg;
+
+const cliOptions = (yargs) => {
+  yargs
+    .positional("database", {
+      description: "Name of database",
+      type: "string",
+    })
+    .positional("user", {
+      description: "Username",
+      type: "string",
+    })
+    .positional("password", {
+      description: "Password for username",
+      type: "string",
+    });
+};
+
+const runner = async (argv) => {
   try {
-    const entries = await getSlugsWithContentList();
+    const pool = new Pool({
+      host: process.env.DICTYCONTENT_BACKEND_SERVICE_HOST,
+      port: process.env.DICTYCONTENT_BACKEND_SERVICE_PORT,
+      database: argv.database,
+      user: argv.user,
+      password: argv.password,
+    });
+    const entries = await getSlugsWithContentList(pool);
     await updateContent(
+      pool,
       entries[1].slug,
       JSON.stringify(convertSlate047(entries[1].content))
     );
+    await pool.end();
     console.log("finished updating content...");
     console.log(`${entries.length} entries were successfully updated`);
   } catch (err) {
     console.log(err);
-    await pool.end();
-  } finally {
-    await pool.end();
+    process.exit(1);
   }
 };
 
-main();
+const main = () => {
+  return yargs(hideBin(process.argv))
+    .demandCommand()
+    .command(
+      "convert [options]",
+      "convert slate 0.47 data to 0.5x format",
+      cliOptions,
+      runner
+    )
+    .demandOption(["database", "user", "password"])
+    .help().argv;
+};
+
+try {
+  main();
+} catch (err) {
+  console.error("something went seriously wrong with the script", err);
+  process.exit(1);
+}
